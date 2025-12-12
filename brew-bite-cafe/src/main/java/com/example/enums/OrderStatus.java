dd726681ada0cf51eb3bef5dc7a22b7a68ea9c69
Mikita Misiulia <nikmisg@gmail.com>
2025-12-12 11:06:28 -0600

    added Gson Maven dependency and fixed the file structure

---
+++ brew-bite-cafe/src/main/java/com/example/enums/OrderStatus.java
@@ -0,0 +1,4 @@
+package com.example.enums;
+public enum OrderStatus {
+    PENDING, IN_PROGRESS, READY, FULFILLED;
+}
---
+++ brew-bite-cafe/src/main/java/com/example/model/CafeModel.java
@@ -0,0 +1,17 @@
+package com.example.model;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.*;
+
+  
+
