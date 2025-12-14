098eb264fd812344478bb8a46515bb34f04ba31b
evankaiser29-debug <evan.kaiser29@gmail.com>
2025-12-14 03:19:27 -0600

    Refactor ManagerMenuController to handle menu actions
    Updated code

--- src/main/java/com/example/controller/ManagerMenuController.java
+++ src/main/java/com/example/controller/ManagerMenuController.java
@@ -1,17 +1,45 @@
 package com.example.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import com.example.model.Menu;
 import com.example.model.Observer;
 
 public class ManagerMenuController implements Observer {
 
-    public ManagerMenuController(){}
+    @FXML
+    private ListView<String> menuList;
+
+    private final Menu menu;
+
+    public ManagerMenuController() {
+        menu = Menu.getInstance();
+        menu.registerObserver(this);
+    }
 
     @Override
     public void update() {
-        // refresh menu view
+        menuList.getItems().setAll(menu.getItems());
     }
 
-    public void handleAdd(){}
-    public void handleEdit(){}
-    public void handleRemove(){}
+    @FXML
+    public void handleAdd() {
+        menu.addItem("New Menu Item");
+    }
+
+    @FXML
+    public void handleEdit() {
+        String item = menuList.getSelectionModel().getSelectedItem();
+        if (item != null) {
+            menu.editItem(item);
+        }
+    }
+
+    @FXML
+    public void handleRemove() {
+        String item = menuList.getSelectionModel().getSelectedItem();
+        if (item != null) {
+            menu.removeItem(item);
+        }
+    }
 }
