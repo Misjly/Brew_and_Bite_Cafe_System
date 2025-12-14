c7671b407ed4a14c2020c482482a9a6c76c23a8a
evankaiser29-debug <evan.kaiser29@gmail.com>
2025-12-14 03:16:52 -0600

    Add FXML annotations to event handler methods
    Updated code

--- src/main/java/com/example/controller/CustomerItemDetailController.java
+++ src/main/java/com/example/controller/CustomerItemDetailController.java
@@ -1,6 +1,24 @@
 package com.example.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CustomerItemDetailController {
-    public void handleCancel(){}
-    public void handleConfirm(){}
+
+    @FXML
+    private Label itemNameLabel;
+
+    @FXML
+    private Label ingredientLabel;
+
+    @FXML
+    private Label costLabel;
+
+    @FXML
+    public void handleCancel() {
+    }
+
+    @FXML
+    public void handleConfirm() {
+    }
 }
