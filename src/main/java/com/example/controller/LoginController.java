19b48a102bb5805ab6b45508768d09f42f037234
evankaiser29-debug <evan.kaiser29@gmail.com>
2025-12-14 03:18:23 -0600

    Update LoginController.java
    Updated code

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
 public class LoginController {
-    public void handleLogin() {}
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    public void handleLogin() {
+        String password = passwordField.getText();
+    }
 }
